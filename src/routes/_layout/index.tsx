aecfaa4ab5f8d01d24aaca7596cb846e10681bb2
Nana Kojo <nkabenyah@gmail.com>
2025-03-24 22:07:45 -0700

    Remove unused image assets and update image paths in layout

--- src/routes/_layout/index.tsx
+++ src/routes/_layout/index.tsx
@@ -71,7 +71,7 @@ function RouteComponent() {
             <div className="group relative">
               <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
                 <img
-                  src="/src/assets/knife-image.jpg"
+                  src="/knife-image.jpg"
                   alt="Professional Chef Knife"
                   className="w-full h-full object-center object-cover lg:w-full lg:h-full"
                 />
@@ -97,7 +97,7 @@ function RouteComponent() {
             <div className="group relative">
               <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
                 <img
-                  src="/src/assets/knife-image.jpg"
+                  src="/knife-image.jpg"
                   alt="Wooden Cutting Board"
                   className="w-full h-full object-center object-cover lg:w-full lg:h-full"
                 />
